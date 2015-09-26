590c31539fc1b60d1fc52e33181533bb7ab194a9
tim_hemmer@gallup.com <tim_hemmer@gallup.com>
2015-09-26 10:00:52 -0500

    Starting Mirage

---
+++ app/mirage/factories/user.js
@@ -0,0 +1,14 @@
+/*
+  This is an example factory definition.
+
+  Create more files in this directory to define additional factories.
+*/
+// import Mirage/*, {faker} */ from 'ember-cli-mirage';
+
+export default [
+  {id: 1, name: 'Bam Bam'},
+  {id: 2, name: 'Fred'},
+  {id: 3, name: 'Wilma'},
+  {id: 4, name: 'Barney'},
+  {id: 5, name: 'Betty'}
+];
